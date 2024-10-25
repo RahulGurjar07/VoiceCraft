7b6f1550e73cb1eb72ae7780be6bf7575d9296fc
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-10-25 11:16:48 +0530

    setup convex clerk and openai components

--- voicecraft/next.config.ts
+++ voicecraft/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
